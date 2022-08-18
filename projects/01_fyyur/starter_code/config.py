5617b931f152664619f8c741c78b916a462e68b3
noelkimwatan <noelkimwatan@gmail.com>
2022-08-18 22:39:12 +0300

    Added environment variables

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -8,7 +8,12 @@ DEBUG = True
 
 # Connect to the database
 
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_NAME = os.getenv('DB_NAME', 'your_db')
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://noelkimwatan@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgresql://{}@{}/{}'.format(DB_USER, DB_HOST, DB_NAME)
+
+# # TODO IMPLEMENT DATABASE URL
+# SQLALCHEMY_DATABASE_URI = 'postgresql://noelkimwatan@localhost:5432/fyyur'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
